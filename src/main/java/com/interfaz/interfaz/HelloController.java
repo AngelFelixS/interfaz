6b04e5e98e6b5da3e14f9764d7e142b1764f52df
Angel Felix <afserranoc01@iesalbarregas.es>
2024-10-09 19:59:11 +0200

    c

--- src/main/java/com/interfaz/interfaz/HelloController.java
+++ src/main/java/com/interfaz/interfaz/HelloController.java
@@ -1,14 +1,18 @@
 package com.interfaz.interfaz;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Button button;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+        button.setText("ewwseds");
     }
 }
