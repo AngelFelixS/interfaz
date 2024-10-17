d57b216e91cb17659cef5ec802ea0f62afcde274
Angel Felix <afserranoc01@iesalbarregas.es>
2024-10-17 19:59:15 +0200

    producto

--- src/main/java/com/interfaz/interfaz/Producto.java
+++ src/main/java/com/interfaz/interfaz/Producto.java
@@ -4,9 +4,18 @@ public class Producto {
     private String nombre;
     private String tipoProducto;
     private boolean procedencia;
-    private boolean iva;
+    private boolean tieneIva;
     private String fechaProduccion;
     private String descripcionProducto;
+
+    public Producto(String nombre, String tipoProducto, boolean procedencia, String fechaProduccion, boolean tieneIva, String descripcionProducto) {
+        this.nombre = nombre;
+        this.tipoProducto = tipoProducto;
+        this.procedencia = procedencia;
+        this.fechaProduccion = fechaProduccion;
+        this.tieneIva = tieneIva;
+        this.descripcionProducto = descripcionProducto;
+    }
 
     public String getNombre() {
         return nombre;
@@ -20,8 +29,8 @@ public class Producto {
         return procedencia;
     }
 
-    public boolean isIva() {
-        return iva;
+    public boolean tieneIva() {
+        return tieneIva;
     }
 
     public String getFechaProduccion() {
