a6d4497a3257d1adb1448ade373e1d9605d5a17d
Mauro <mauromayoralxd@gmail.com>
2024-10-21 20:44:50 +0200

    Consultas

--- src/main/java/com/interfaz/interfaz/Modelo.java
+++ src/main/java/com/interfaz/interfaz/Modelo.java
@@ -5,10 +5,21 @@ import java.util.ArrayList;
 
 public class Modelo {
     public static ArrayList listaClientes;
+    public static ArrayList listaProductos;
+    public static ArrayList listaProveedor;
 
 
     public static boolean crearCliente(Cliente c){
         listaClientes.add(c);
+        return true;
+    }
+
+    public static boolean crearProyecto(Producto p){
+        listaClientes.add(p);
+        return true;
+    }
+    public static boolean crearProveedor(Proveedor pr){
+        listaClientes.add(pr);
         return true;
     }
 }
