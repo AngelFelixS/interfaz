86a39bbef872e8fbba1f752cbc02cbbb76d7263c
Antonio <tonetecm1@gmail.com>
2024-10-23 19:37:46 +0200

    darAltaClienteReal

--- src/main/java/com/interfaz/interfaz/ControladorCliente.java
+++ src/main/java/com/interfaz/interfaz/ControladorCliente.java
@@ -4,13 +4,28 @@ import javafx.animation.PauseTransition;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.util.Duration;
 
 public class ControladorCliente extends ControladorPrincipal {
     @FXML
     private ComboBox esEmpleado;
+    @FXML
+    private CheckBox jubilado;
+    @FXML
+    private RadioButton hombre;
+    @FXML
+    private RadioButton mujer;
+    @FXML
+    private TextField nombre;
+    @FXML
+    private TextField dni;
+    @FXML
+    private TextField correoElectronico;
+    @FXML
+    private DatePicker fechaNacimiento;
+    @FXML
+    private TextArea datosExtra;
     @FXML
     protected void inicializar(){
         String[] opciones = {"Si","No"};
@@ -21,6 +36,8 @@ public class ControladorCliente extends ControladorPrincipal {
     protected void onConfirmarClick(ActionEvent event) {
         Label mensajeLabel = new Label("Cliente dado de alta con éxito");
         mensajeConfirmar.getChildren().add(mensajeLabel);
+        Cliente c=new Cliente(nombre.getText(),dni.getText(),esEmpleado.getValue().toString(),hombre.isSelected(), correoElectronico.getText(),fechaNacimiento.getValue().toString(),jubilado.isSelected(),datosExtra.getText());
+        HelloApplication.getModelo().darAlta(c);
         PauseTransition pause = new PauseTransition(Duration.seconds(1));
         pause.setOnFinished(e -> {
             mensajeConfirmar.getChildren().remove(mensajeLabel);
