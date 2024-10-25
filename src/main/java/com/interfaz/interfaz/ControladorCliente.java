f917989b9c7119aab9a79bf0b4a69dee682027ac
Antonio <tonetecm1@gmail.com>
2024-10-25 11:54:42 +0200

    Baja

--- src/main/java/com/interfaz/interfaz/ControladorCliente.java
+++ src/main/java/com/interfaz/interfaz/ControladorCliente.java
@@ -11,7 +11,12 @@ import javafx.util.Duration;
 
 public class ControladorCliente extends ControladorPrincipal {
 
-    public VBox confirmarBaja;
+    @FXML
+    private VBox confirmarBaja;
+    @FXML
+    private Button cancelarButton;
+    @FXML
+    private Button confirmarButton;
     @FXML
     private ComboBox esEmpleado;
     @FXML
@@ -62,34 +67,21 @@ public class ControladorCliente extends ControladorPrincipal {
 
     @FXML
     protected void darBajaCliente() {
-        String dniActual = dni.getText();  // Obtenemos el DNI ingresado.
-        Cliente clienteAEliminar = null;
+        confirmarBaja.setOpacity(1);
+        confirmarBaja.setDisable(false);
 
-
-        for (Cliente cliente : modelo.getListaClientes()) {
-            if (cliente.getDni().equals(dniActual)) {
-                clienteAEliminar = cliente;
-                break;
-            }
-        }
-
-        if (clienteAEliminar != null) {
-            modelo.darBaja(clienteAEliminar);
-            System.out.println("Cliente eliminado: " + clienteAEliminar);
-
-            confirmarBaja.setOpacity(1);
-            confirmarBaja.setDisable(false);
-
-            PauseTransition pause = new PauseTransition(Duration.seconds(1.5));
-            pause.setOnFinished(e -> {
-                confirmarBaja.setOpacity(0);
-                confirmarBaja.setDisable(true);
-            });
-            pause.play();
-        } else {
-            System.out.println("Cliente no encontrado con DNI: " + dniActual);
-
-        }
+        cancelarButton.setOnAction(e -> ocultarConfirmacion());
+        confirmarButton.setOnAction(e -> {ocultarConfirmacion();
+        });
     }
-
+    private void ocultarConfirmacion() {
+        PauseTransition pause = new PauseTransition(Duration.seconds(1));
+        pause.setOnFinished(e -> {
+            confirmarBaja.setOpacity(0);
+            confirmarBaja.setDisable(true);
+            confirmarBaja.getParent().setOpacity(0);
+            confirmarBaja.getParent().setDisable(true);
+        });
+        pause.play();
+    }
 }
